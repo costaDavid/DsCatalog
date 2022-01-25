2388de537ac56af89313ccb1014b324e7c6911ac
Fabio Patricio <fabio.patricio@gruposlc.onmicrosoft.com>
2022-01-25 11:25:59 -0300

    Auditing Data

--- src/main/java/com/Costa/DsCatalog/entity/Category.java
+++ src/main/java/com/Costa/DsCatalog/entity/Category.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
@@ -21,4 +22,20 @@ public class Category implements Serializable{
     private Long id;
 
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
 }
